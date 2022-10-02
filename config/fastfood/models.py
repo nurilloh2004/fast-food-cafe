dc26e5d561fe0430cf5b68b6c063f13a092d62b2
nurilloh2004 <nurillohpython@gmail.com>
2022-10-02 13:37:52 +0500

    *******

--- config/fastfood/models.py
+++ config/fastfood/models.py
@@ -3,12 +3,16 @@ from django.contrib.auth.models import User
 from .utils import *
 # Create your models here.
 
-class User(DataInfo):
+class User(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=100, null=True)
     email = models.EmailField(null=True)
     profile_pic = models.ImageField(null=True)
+    release_date = models.DateField()
 
     def __str__(self):
         return self.name
+
+
+
